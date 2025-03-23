e1b332d6d890e38167547f3f709f10d6ac073ef5
devjatin <jatinrawat27official@gmail.com>
2025-03-23 15:42:37 +0530

    first project ✅

---
+++ react-helloworld/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen bg-blue-200'>
+      <button className='bg-blue-500 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 p-4 text-2xl rounded-full text-white font-medium'>Hello world , written by Jatin!</button>
+      </div>
+    </>
+  )
+}
+
+export default App
